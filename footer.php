5cda389919c142e3fa301d8e37e48c424cf93da0
PetaDigi Dev <user@petadigi.local>
2026-07-27 16:44:50 +0700

    feat: Update footer social links and correct contact email

--- footer.php
+++ footer.php
@@ -6,12 +6,12 @@
                 <a href="#" target="_blank" title="TikTok"><i class="fa-brands fa-tiktok"></i></a>
                 <a href="#" target="_blank" title="Instagram"><i class="fa-brands fa-instagram"></i></a>
                 <a href="#" target="_blank" title="Facebook"><i class="fa-brands fa-facebook-f"></i></a>
-                <a href="#" target="_blank" title="X (Twitter)"><i class="fa-brands fa-x"></i></a>
+                <a href="#" target="_blank" title="X" class="social-x"><svg viewBox="0 0 24 24" width="16" height="16" fill="currentColor" aria-hidden="true"><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/></svg></a>
             </div>
         </div>
         <div class="footer-right">
             <h3>Hubungi Kami</h3>
-            <p>lexplna.help@gmail.com</p>
+            <p>lexpina.help@gmail.com</p>
             <p>+62 82135840605</p>
             <p>Jl. Pengadegan Barat VII No. E/3, Pengadegan, Pancoran, Jakarta Selatan.</p>
             <button class="btn-saran" onclick="window.location.href='saran.php'">Saran & Masukan</button>
